7090949db5ad500d688602e70f059a43750fa976
savnnaka <karin-haslauer@gmx.de>
2023-03-09 14:42:02 +0100

    Details Section

--- mp_karin/src/Components/Details.jsx
+++ mp_karin/src/Components/Details.jsx
@@ -1,72 +1,139 @@
 import { Typography} from "@mui/material";
-import { Box, Container, styled } from "@mui/system";
+import { Box, Container, Stack, styled } from "@mui/system";
 import Votingbox from './Votingbox';
 import handyBlank from '../img/handyBlank.png';
 
-const ColumnReverseBox = styled(Box)(({ theme }) => ({
+const DetailsBox = styled(Box)(({theme}) => ({
+    backgroundColor: "#141414",
+    backgroundImage: `url(${handyBlank})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "auto 80%",
+    backgroundPosition: "center 40%",
+    margin: "0, 0",
+}));
+
+const CustomStack = (props) => {
+    return (
+        <Stack 
+        direction={{ sm: 'column', md: 'row' }} 
+        alignItems="center"
+        spacing={{ sm: 0, md: 3}}
+        sx={{margin:"20px 20%"}}
+    >
+        <Typography variant="h5" sx={{color: "white", whiteSpace: "nowrap"}}>{props.text}</Typography>
+        <Box sx={{textAlign: "center"}}>
+            <img
+                src={props.img}
+                alt="handy Image"
+                style={{ maxWidth: "80%"}}
+            />
+        </Box>
+
+    </Stack>
+    );
+}
+
+const CustomReverseStack = (props) => {
+    return (
+        <Stack 
+        direction={{ sm: 'column-reverse', md: 'row' }} 
+        alignItems="center"
+        spacing={{ sm: 0, md: 3}}
+        sx={{margin:"20px 20%"}}
+    >
+        <Box sx={{textAlign: "center"}}>
+            <img
+                src={props.img}
+                alt="handy Image"
+                style={{ maxWidth: "80%"}}
+            />
+        </Box>
+        <Typography variant="h5" sx={{color: "white", whiteSpace: "nowrap"}}>{props.text}</Typography>
+
+    </Stack>
+    );
+}
+
+
+
+
+const TextStack = () => {
+    return (
+        <Stack 
+        direction={{ sm: 'column', md: 'row' }} 
+        spacing={{ md: 2 }}
+        sx={{color: "#d4fc04", whiteSpace: "nowrap"}}
+        />
+    );
+}
+
+const CustomBox = styled(Box)(({ theme }) => ({
+    marginBottom: "2rem",
     display: "flex",
-    alignItems: "center",
-    // justifyContent: "space-between",
+    gap: theme.spacing(15),
     [theme.breakpoints.down("md")]: {
-      flexDirection: "column-reverse",
-      textAlign: "center",
+        gap: theme.spacing(0),
+        flexDirection: "column",
+        alignItems: "center",
+        textAlign: "center",
     },
-  }));
+}));
+
+const ColumnReverseBox = styled(Box)(({ theme }) => ({
+    marginBottom: "2rem",
+    display: "flex",
+    justifyContent: "center",
+    gap: theme.spacing(15),
+    [theme.breakpoints.down("md")]: {
+        gap: theme.spacing(0),
+        flexDirection: "column-reverse",
+        alignItems: "center",
+        textAlign: "center",
+    },
+}));
 
 const CustomBox2 = styled(Box)(({ theme }) => ({
+    marginBottom: "2rem",
     display: "flex",
     alignItems: "center",
-    // justifyContent: "space-between",    
+    justifyContent: "space-between",    
     [theme.breakpoints.down("md")]: {
       flexDirection: "column",
       textAlign: "center",
     },
-  }));
+}));
+
 
 const Details = () => {  
     return (
-        <Box sx={{backgroundColor: "#141414", margin: "0, 0"}}>
-            <Typography variant="h4" sx={{color: "#d4fc04"}}>Kommt dir das bekannt vor?</Typography>
-            <Box sx={{m: "0 10%"}}>
-                <ColumnReverseBox>
-                    <Box sx={{flex: 1}}>
-                    <img
-                        src={require("../img/image(2).png")}
-                        alt="handy Image"
-                        style={{ maxWidth: "50%", marginBottom: "2rem" }}
-                    />
-                    </Box >
-                    <Typography variant="h5" sx={{color: "white"}}>Kurzfristige Absagen</Typography>
-                </ColumnReverseBox>
-                <CustomBox2>
-                    <Typography variant="h5" sx={{color: "white"}}>Kurzfristige Absagen</Typography>
-                    <Box sx={{flex: 1}}>
-                        <img
-                            src={require("../img/image(2).png")}
-                            alt="handy Image"
-                            style={{ maxWidth: "50%", marginBottom: "2rem" }}
-                        />
-                    </Box>
-                </CustomBox2>
-                <ColumnReverseBox>
-                    <Box sx={{flex: 1}}>
-                        <img
-                            src={require("../img/image(2).png")}
-                            alt="handy Image"
-                            style={{ maxWidth: "50%", marginBottom: "2rem" }}
-                        />
-                    </Box>
-                    <Typography variant="h5" sx={{color: "white"}}>Kurzfristige Absagen</Typography>
-                </ColumnReverseBox>
+        <DetailsBox>
+            <Typography variant="h4" 
+                sx={{color: "#d4fc04", marginBottom: "10%"}}
+            >
+                Kommt dir das bekannt vor?
+            </Typography>
+
+            <Box sx={{m: "10% 10%"}}>
+
+                <CustomReverseStack img={require("../img/image(2).png")} text="Kurzfristige"/>
+
+                <CustomStack img={require("../img/image(2).png")} text="Kurzfristige Absagen"/>
+
+                <CustomReverseStack img={require("../img/image(2).png")} text="Hallao"/>
+
+                <CustomStack img={require("../img/image(2).png")} text="Kurzfristige Absagen"/>
+
+                <CustomReverseStack img={require("../img/image(2).png")} text="Hallao"/>
+
             </Box>
-            <CustomBox2 sx={{justifyContent: "space-between", m: "0 10%"}}>
-                <Box sx={{display: "flex", gap: 1, color: "#d4fc04"}}>
+            <CustomBox2>
+                <TextStack>
                     <Typography variant="h4">Als Coach musst Du</Typography>
                     <Typography variant="h4" sx={{fontWeight: "bold"}}>ALLES MACHEN.</Typography>
-                </Box>
+                </TextStack>
                 <Votingbox />
             </CustomBox2>
-        </Box>
+        </DetailsBox>
     );
   };    
 
--- mp_karin/src/Components/Votingbox.jsx
+++ mp_karin/src/Components/Votingbox.jsx
@@ -1,12 +1,12 @@
-import Counter from "./Counter";
-import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
-import ThumbDownOffAltIcon from '@mui/icons-material/ThumbDownOffAlt';
 import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+import ThumbDownOffAltIcon from '@mui/icons-material/ThumbDownOffAlt';
+import Counter from "./Counter";
 
 const Votingbox = () => {
     return (
-        <Box sx={{borderRadius: "5px", border: '1px solid #d4fc04', p: "5px 13px"}}>
+        <Box sx={{borderRadius: "5px", border: '1px solid #d4fc04', p: "5px 13px", textAlign:"center"}}>
             <Typography variant="h5" sx={{color: "#d4fc04"}}>Geht's Dir auch so?</Typography>
             <Box sx={{display: "flex", alignItems: "center", m:"0 20%"}}>
                 <Counter icon={<ThumbUpOffAltIcon />} c={"white"}></Counter>
